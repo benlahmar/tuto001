ca331b2a1c6a876ed56bfadf28efcf1c7a5f743e
h.benlahmer <h.benlahmer@gmail.com>
2024-02-13 14:43:14 +0100

    add

--- Isga1/src/main/java/com/dao/IUser.java
+++ Isga1/src/main/java/com/dao/IUser.java
@@ -6,7 +6,7 @@ import com.model.User;
 
 public interface IUser {
 
-	public int adduser(User u);
+	public int adduser(User u); 
 	public int adduser(String lg,String ps);
 	public User findById(int id);
 	List<User> allUsers();
